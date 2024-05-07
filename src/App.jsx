d35e852e7b4939f06c4e129649d7bbfae1209aaa
Abdusamad Latipov <abdusamadlatipov67@gmail.com>
2024-05-07 21:46:27 +0500

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from "./components/Login/Login";
 import SignUp from "./components/SignUp/SignUp";
-
+import Sidebar from './components/Sidebar/Sidebar';
 function App() {
     return (
         <Router>
             <div className="login">
                 <Routes>
-                    <Route path="/" element={<Login />} />
+                    <Route path='/' element={<Sidebar/>}/>
+                    <Route path="/login" element={<Login />} />
                     <Route path="/signup" element={<SignUp />} />
                 </Routes>
             </div>
