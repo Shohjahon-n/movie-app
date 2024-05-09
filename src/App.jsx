18fc4dce4a2f6bc0e7d4495eabd9cec7588da8d7
Shohjahon-n <pazvat56@gmail.com>
2024-05-09 17:00:47 +0500

    fixed backend

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import Login from "./components/Login/Login";
 import SignUp from "./components/SignUp/SignUp";
 import Sidebar from './components/Sidebar/Sidebar';
 import Search from './components/Search/Search'
-import Axios from 'axios';
+import axios from 'axios';
 import { useState, useEffect } from 'react';
 function App() {
-    const [data, setData] = useState([]);
+    const [data, setData] = useState(null);
 
     useEffect(() => {
-        Axios.get('http://localhost:5000/data')
+        axios.get('http://localhost:5000/data')
             .then(response => {
                 setData(response.data);
             })
@@ -17,14 +17,17 @@ function App() {
                 console.error('Error fetching data:', error);
             });
     }, []);
-    console.log(data);
+
+    if (!data) {
+        return <div>Loading...</div>;
+    }
+
     return (
         <Router>
             <div className="login">
                 <div style={{ display: 'flex', alignItems: 'flex-start', columnGap: '30px' }}>
                     <Sidebar />
                     <Search />
-                    <h1>{data[0].name}</h1>
                 </div>
                 <Routes>
                     <Route path="/login" element={<Login />} />
