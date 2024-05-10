5f6641906f0057a6f67d1de2132ced524ecca7e0
Shohjahon-n <pazvat56@gmail.com>
2024-05-10 19:57:56 +0500

    added search

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,35 @@ import Search from './components/Search/Search'
 import { useState, useEffect } from 'react';
 function App() {
     const [data, setData] = useState([]);
+    const [value, setValue] = useState('');
+    const [lodading, setLoading] = useState(true);
+    const [filteredData, setFilteredData] = useState([]);
 
     useEffect(() => {
         fetch('https://server2425.onrender.com/data')
             .then(res => res.json())
             .then(data => {
                 setData(data);
+                setFilteredData(data);
+                setLoading(false);
             }).catch(err => console.log(err))
     }, []);
 
     console.log(data);
+    const handleSearch = (e) => {
+        e.preventDefault()
+        setValue(e.target.value);
+        const filteredData = data.filter(item => item.title.toLowerCase().includes(value.toLowerCase()));
+        setFilteredData(filteredData);
+    }
 
     return (
         <Router>
             <div className="login">
                 <div style={{ display: 'flex', alignItems: 'flex-start', columnGap: '30px' }}>
                     <Sidebar />
-                    <Search data={data} />
+                    <Search loading={lodading} value={value} handleSearch={handleSearch}
+                        filteredData={filteredData} data={data} />
                 </div>
                 <Routes>
                     <Route path="/login" element={<Login />} />
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -2,15 +2,18 @@ import search from '../../assets/image/search.png'
 import './Search.scss'
 import Trending from '../Trending/Trending'
 import Cardlist from '../Cardlist/Cardlist'
-export default function Search({ data }) {
+export default function Search({ data, value, handleSearch, loading, filteredData }) {
     return (
         <div className='search_content container'>
             <div className='input_content'>
-                <img src={search} alt="search" />
-                <input type="text" placeholder='Search for movies or TV series' />
+                <form >
+                    <img src={search} alt="search" />
+                    <input value={value} onChange={handleSearch} type="text" placeholder='Search for movies or TV series' />
+                </form>
             </div>
             <Trending data={data} />
-            <Cardlist data={data}/>
+            {loading ? <h1>Loading...</h1> : null}
+            <Cardlist data={filteredData} />
         </div>
     )
 }
